4e56f4e544d34091aa38886877966f5337f8b02a
Svetlyi <svetlyi62@gmail.com>
2014-11-28 23:39:43 +0300

    new cubes

--- Assets/KeyHandler.cs
+++ Assets/KeyHandler.cs
@@ -4,9 +4,19 @@ using System.Collections;
 public class KeyHandler : MonoBehaviour {
 	public GameObject light;
 	public float speed = 0.1f;
+	private GameObject[] chain = new GameObject[100];
+	private int chainCounter = 0;
+
 	// Use this for initialization
 	void Start () {
-		Debug.Log("I am alive!");
+		//GameObject newCube = new 
+		for (int i = 0; i < 5; i++) {
+			GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+			sphere.AddComponent<Rigidbody>().mass = 0.0002f;
+			sphere.transform.position = new Vector3(i*3, 10, i*2);
+			sphere.name = "sphere" + i.ToString();
+			//GameObject light = GameObject.CreatePrimitive(PrimitiveType.s
+		}
 	}
 	
 	// Update is called once per frame
@@ -20,22 +30,43 @@ public class KeyHandler : MonoBehaviour {
 		this.keyHandler ();
 	}
 
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.name != "Plane") {
+			chain[chainCounter] = col.gameObject;
+			if (chainCounter > 1) {
+				chain[chainCounter].AddComponent<FixedJoint>().connectedBody = col.gameObject.rigidbody;
+			} else {
+				this.gameObject.AddComponent<FixedJoint>().connectedBody = col.gameObject.rigidbody;
+			}
+			chainCounter ++;
+		}
+		//Destroy (col.gameObject);
+	}
+
 	private void keyHandler() {
 		Vector3 oldObjPos = transform.position;
 		//GameObject game = GameObject.Find("Plane");
 		if (Input.anyKey) {
 			oldObjPos = transform.position;
 			if (Input.GetKey ("right")) {
-					oldObjPos.x += this.speed;
+				//oldObjPos.x += this.speed;
+				this.gameObject.rigidbody.AddForce(10, 0, 0);
+				//transform.Translate(0,0,10);
 			}
 			if (Input.GetKey ("left")) {
-					oldObjPos.x -= this.speed;	
+					oldObjPos.x -= this.speed;
+				//this.gameObject.rigidbody.AddForce(0, -10, 0);
 			}
 			if (Input.GetKey ("up")) {
-					oldObjPos.z += this.speed;		
+					oldObjPos.z += this.speed;	
+				//this.gameObject.rigidbody.AddForce(10, 0, 0);
 			}
 			if (Input.GetKey ("down")) {
 					oldObjPos.z -= this.speed;	
+				//this.gameObject.rigidbody.AddForce(-10, 0, 0);
+			}
+			if (Input.GetKey ("space")) {
+				this.gameObject.rigidbody.AddForce(0, 40, 0);
 			}
 			transform.position = oldObjPos;	
 		}
