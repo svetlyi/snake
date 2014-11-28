c86d9760309ba52e78467972051f4953e6ce77f1
Svetlyi <svetlyi62@gmail.com>
2014-11-28 22:01:53 +0300

    Key handling

--- Assets/KeyHandler.cs
+++ Assets/KeyHandler.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class KeyHandler : MonoBehaviour {
 	public GameObject light;
+	public float speed = 0.1f;
 	// Use this for initialization
 	void Start () {
 		Debug.Log("I am alive!");
@@ -16,8 +17,32 @@ public class KeyHandler : MonoBehaviour {
 		tempLightPos.y = light.transform.position.y;
 		light.transform.position = tempLightPos;
 
-		tempLightPos = transform.position;
-		tempLightPos.x += 0.1f;
-		transform.position = tempLightPos;
+		this.keyHandler ();
+	}
+
+	private void keyHandler() {
+		Vector3 oldObjPos = transform.position;
+		//GameObject game = GameObject.Find("Plane");
+		if (Input.anyKey) {
+			oldObjPos = transform.position;
+			if (Input.GetKey ("right")) {
+					oldObjPos.x += this.speed;
+			}
+			if (Input.GetKey ("left")) {
+					oldObjPos.x -= this.speed;	
+			}
+			if (Input.GetKey ("up")) {
+					oldObjPos.z += this.speed;		
+			}
+			if (Input.GetKey ("down")) {
+					oldObjPos.z -= this.speed;	
+			}
+			transform.position = oldObjPos;	
+		}
+	}
+
+	void OnGUI() {
+		Rect rect = new Rect (10,10,150,100);
+		GUI.Label(rect, "Some information about this game!!!");
 	}
 }
